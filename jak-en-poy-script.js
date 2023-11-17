dd37bd81ab972954130e9f3cbb1536d251cd943c
tepcodes <123259410+tepcodes@users.noreply.github.com>
2023-11-17 15:54:18 +0800

    Add script

---
+++ jak-en-poy-script.js
@@ -0,0 +1,37 @@
+let choices = ['rock' , 'paper' , 'scissors'];
+let anotherRound = true;
+
+while (anotherRound) {
+    let randomChoice = Math.floor(Math.random() * choices.length)
+    let computerChoice = choices[randomChoice];
+    let playerChoice = prompt('rock, paper, scissors?');
+
+    function determineResult (computer, player) {
+        if (computer === player) {
+            return('Draw!');
+        } else if (
+            (computer === 'rock' && player === 'scissors') ||
+            (computer === 'paper' && player === 'rock') ||
+            (computer === 'scissors' && player === 'paper')
+        ) {
+            return('You lose!');
+        } else if (
+            (player === 'rock' && computer === 'scissors') ||
+            (player === 'paper' && computer === 'rock') ||
+            (player === 'scissors' && computer === 'paper')
+        ) {
+            return('You win!');
+        } else {
+            return ('Invalid choice. Choose again.')
+        }
+    }
+
+    function round () {
+        let result = determineResult(computerChoice,playerChoice);
+        alert(result);
+    };
+
+    round();
+
+    anotherRound=confirm('Another round!');
+}
