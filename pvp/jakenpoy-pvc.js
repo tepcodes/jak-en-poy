bb9ecce6475d6700a79a75fa92502726a4862822
tepcodes <123259410+tepcodes@users.noreply.github.com>
2023-11-24 17:31:50 +0800

    Add main function for game logic

--- pvp/jakenpoy-pvc.js
+++ pvp/jakenpoy-pvc.js
@@ -1,51 +1,121 @@
-//Goal of game
-    //Hit the opponent until his HP reaches 0.
-//Rules of game
-    //Each player starts with 50 HP.
-    //The round starts by selecting an action.
-    //Based on the action chosen, damage calculation takes place.
-    //The first player to reach 0 HP loses.
-    //The game ends when a player has 0 HP.
-//Main game logic
-    //Players
-        //User is the user.
-        //Computer is the computer.
-    //Each action corresponds to a change in HP:
-        //Attack - deals 10 dagame to opponent's HP.
-        //Defend - negates any changes to your HP.
-        //Heal - adds 5 to your HP.
-    //User selects an action.
-        //User clicks a button for the action.
-    //Computer selects an action.        
-        //Random selection via Math.random()
-    //Damage calculation (player, action):
-        //User Attack && Computer Attack
-            //User -10 HP, Computer -10 HP.
-        //User Attack && Computer Defend
-        //User Defend && Computer Attack
-        //User Defend && Computer Defend
-            //No HP change.
-        //User Attack && Computer Heal
-            //User no change HP, Computer -10 HP +5 HP.
-        //User Defend && Computer Heal
-            //User no change HP, Computer +5 HP.
-        //User Heal && Computer Attack
-            //User +5 HP -10 HP, Computer no change HP.
-        //User Heal && Computer Defend
-            //User +5 HP, Computer no change HP.
-        //User Heal && Computer Heal
-            //User +5 HP, Computer +5 HP.
-    //Game result
-        //If User HP <= 0 && Computer HP <= 0 "Draw!"
-        //If User HP > 0 && Computer HP <= 0 "You win!"
-        //If User HP <= 0 && Computer HP > 0 "Computer wins!"
-    //Reset game
-        //If game result is displayed, disable all the action buttons and enable the restart game button.
-//Display message
-    //Display actions taken ("User (action)).
-    //Display damage calcutaion.
-    //Display remaining HP.
-    //Display result.
+// Global variables to store health points
+let userHealthPoints = 50;
+let monsterHealthPoints = 50;
+let monsterAction = '';
+
+// Function to choose a random action for the monster
+function chooseMonsterAction() {
+    let actions = ['Attack', 'Defend', 'Heal'];
+    let actionIndex = Math.floor(Math.random() * actions.length);
+    monsterAction = actions[actionIndex];
+}
+
+// Function to get user action based on the clicked button
+function getUserAction(action) {
+    calculateDamage(action, monsterAction);
+}
+
+// Function to get a random monster action and play the game
+function playGame() {
+    // Randomize monster action for each iteration
+    chooseMonsterAction();
+}
+    // Event listeners for user actions (assuming buttons with ids 'attack', 'defend', 'heal')
+    document.getElementById('attack').addEventListener('click', function () {
+        getUserAction('Attack');
+    });
+
+    document.getElementById('defend').addEventListener('click', function () {
+        getUserAction('Defend');
+    });
+
+    document.getElementById('heal').addEventListener('click', function () {
+        getUserAction('Heal');
+    });
 
 
-//
+// Call playGame() to start the initial game
+playGame();
+
+// Function to calculate damage and update health points
+function calculateDamage(userAction, monsterAction) {
+    const attackAction = 5;
+    const healAction = 10;
+    chooseMonsterAction();
+
+    if (userAction === 'Attack' && monsterAction === 'Attack') {
+        userHealthPoints -= attackAction;
+        monsterHealthPoints -= attackAction;
+        updateHP();
+        displayGameLog(userAction, monsterAction);
+    } else if (
+        userAction === 'Attack' && monsterAction === 'Defend' ||
+        userAction === 'Defend' && monsterAction === 'Defend' ||
+        userAction === 'Defend' && monsterAction === 'Attack'
+    ) {
+        // No change in health points
+        updateHP();
+        displayGameLog(userAction, monsterAction);
+    } else if (userAction === 'Attack' && monsterAction === 'Heal') {
+        monsterHealthPoints += healAction;
+        monsterHealthPoints -= attackAction;
+        updateHP();
+        displayGameLog(userAction, monsterAction);
+    } else if (userAction === 'Defend' && monsterAction === 'Heal') {
+        monsterHealthPoints += healAction;
+        updateHP();
+        displayGameLog(userAction, monsterAction);
+    } else if (userAction === 'Heal' && monsterAction === 'Attack') {
+        userHealthPoints += healAction;
+        userHealthPoints -= attackAction;
+        updateHP();
+        displayGameLog(userAction, monsterAction);
+    } else if (userAction === 'Heal' && monsterAction === 'Defend') {
+        userHealthPoints += healAction;
+        updateHP();
+        displayGameLog(userAction, monsterAction);
+    } else if (userAction === 'Heal' && monsterAction === 'Heal') {
+        userHealthPoints += healAction;
+        monsterHealthPoints += healAction;
+        updateHP();
+        displayGameLog(userAction, monsterAction);
+    }
+}
+
+// Function to update health points on the UI
+function updateHP() {
+    updateUserHP();
+    updateMonsterHP();
+    checkGameEnd(); // Check if the game should end after updating health points
+}
+
+function updateUserHP() {
+    document.querySelector('#userHP').textContent = `Your HP: ${userHealthPoints}`;
+}
+
+function updateMonsterHP() {
+    document.querySelector('#monsterHP').textContent = `Monster HP: ${monsterHealthPoints}`;
+}
+
+// Function to check game end conditions and restart the game if needed
+function checkGameEnd() {
+    if (userHealthPoints <= 0 || monsterHealthPoints <= 0) {
+        userHealthPoints = 50;
+        monsterHealthPoints = 50;
+        document.querySelector('#gameResult').textContent = `Game Over! Restarting...`;
+        playGame(); // Restart the game
+    }
+}
+
+// Function to display game log with the latest entry at the top
+function displayGameLog(userAction, monsterAction) {
+    let gameLogElement = document.getElementById('gameLog');
+    let logEntry = document.createElement('p');
+    logEntry.textContent = `The user ${userAction.toLowerCase()}ed. The monster ${monsterAction.toLowerCase()}ed.`;
+    gameLogElement.insertBefore(logEntry, gameLogElement.firstChild);
+    
+    // Limit the number of paragraphs to a maximum of 10
+    if (gameLogElement.childElementCount > 10) {
+        gameLogElement.removeChild(gameLogElement.lastChild);
+    }
+}
